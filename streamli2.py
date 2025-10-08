fe70b37b175345c1af991ea9cbe288b314a7a158
ahmadddnew193-gif <ahmadddnew193@gmail.com>
2025-10-08 11:23:21 +0300

    Update streamli2.py

--- streamli2.py
+++ streamli2.py
@@ -93,7 +93,7 @@ if st.checkbox("Check Roblox Status"):
             st.error(f"Error checking status: {e}")
 
         time.sleep(delay)
-        st.experimental_rerun()
+        st.rerun()
 
 
 
@@ -153,6 +153,7 @@ if checkbox:
                     st.error(f"Error: {e}") 
             st.dataframe(available_user)
             STOp = True           
+
 
 
 
